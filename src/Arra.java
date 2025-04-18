4207a493e2fa3bc6bb645b108ce811c3f1e79fff
dhara <dharanimoorthy51@gmail.com>
2025-04-18 10:14:26 +0530

    Base

---
+++ src/Arra.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Arra {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {3, 4, 5, 6, 7},
+                {4, 5, 6},
+                {5, 6, 3, 4},
+        };
+        for (int row = 0; row < arr.length; row++) {
+                System.out.println(Arrays.toString(arr[row]));
+            }
+
+        }
+    }
+
+
