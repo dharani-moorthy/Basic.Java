4990450b5ae343080abfbccc917bb449b220d585
dhara <dharanimoorthy51@gmail.com>
2025-05-01 20:02:01 +0530

    PrimeNumber

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,12 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count=0;
+        for (int i = 2; i <10 ; i++) {
+            if(i%2!=0) {
+                count++;
+            }
+            }
+        System.out.println(count);
+        }
+    }
+
